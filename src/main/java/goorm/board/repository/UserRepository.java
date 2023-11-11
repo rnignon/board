87224de4d06eeb6d9319484f2a879c23e5ff0166
rnignon <rnignon@naver.com>
2023-11-12 03:13:26 +0900

    Add: create Repositories

---
+++ src/main/java/goorm/board/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package goorm.board.repository;
+
+import goorm.board.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
